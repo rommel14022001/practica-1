fc4c0cee69b823e2ae2c31300c92970ef69a57ee
samuel <samueliassg@gmail.com>
2022-09-04 19:10:15 -0400

    otro

--- ejercicio.js
+++ ejercicio.js
@@ -1,12 +1,2 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 console.log('holaaaa');
 console.log('como estás?');
-=======
-console.log('chipi');
->>>>>>> 89f978c66e01f0e43fa51b7aa3e0518985898273
-=======
-
-console.log('como estás?');
-console.log('chipi');
->>>>>>> 9eef7d3c6ea6f8336026b541fd3ae48765447d17
